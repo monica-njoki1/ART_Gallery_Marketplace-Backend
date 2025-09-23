7206006eaa7a16c8f7fb3e9d379be291dc0ac6b5
Micheni58 <michenicaleb@gmail.com>
2025-09-23 11:50:56 +0300

    Add models and routes

--- Server/app.py
+++ Server/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from models import db
+from flask import Flask,request,make_response
+from models import db,Artist,Artwork
 from flask_migrate import Migrate
 
 app = Flask(__name__)
@@ -13,6 +13,53 @@ db.init_app(app)
 @app.route("/")
 def home():
     return "Welcome to the Art Gallery Marketplace!"
+
+
+@app.route("/artists")
+def artists():
+    artists = Artist.query.all()
+    artist_list = [artist.to_dict for artist in artists]
+    resp = make_response(artist_list, 200)
+    return resp
+
+
+@app.route("/artists/<int:id>", methods=["GET","DELETE"])
+def artists_by_id(id):
+    artist = Artist.query.filter_by(id ==id).first()
+    if not artist:
+        return {"error":"Artist not found"}
+    if request.method == "GET":
+        return make_response(
+            artist.to_dict(
+                only=("id","name","bio")
+            ),
+            200
+        )
+    if request.method == "DELETE":
+        db.session.delete(artist)
+        db.session.commit()
+        resp = make_response(
+            {"message":"Artist successfully deleted"}
+
+        )
+        return resp
+
+@app.route("/artworks")
+def artworks():
+    artwork = Artwork.query.all()
+    artwork_list = [art.to_dict() for art in artwork]
+    resp = make_response(
+        artwork_list,
+        200
+    )
+    return resp
+# The route below will show the artwork details
+@app.route("/artworks/")
+def art_work_details():
+    pass
+
+# @app.route("")
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5555)
--- Server/models.py
+++ Server/models.py
@@ -1,3 +1,25 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask,Columns,relationships
 
 db = SQLAlchemy()
+
+class Artist(db.Model,):
+    __tablename__ = "artists"
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String)
+    bio = db.Column(db.String)
+    artist = db.relationship("Artist", back_populates="artist")
+    def __repr__(self):
+        return f"<Artist {self.id} {self.name} {self.bio}"
+
+class Artwork(db.Model):
+    __tablename__ = "artworks"
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String,nullable = False)
+    price = db.Column(db.Integer,nullable= False)
+    artist_id = db.Column(db.Integer, foreignKey("artist.id"))
+
+    artist = db.relationship("Artwork", back_populates = "artwork")
+    def __repr__(self):
+        return f"<Artwork {self.id} {self.title} {self.price}"
+    
